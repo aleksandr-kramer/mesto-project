815860249f8cc4fec771ae09a0f86102082b1ccd
aleksandr-kramer <kramer4507@gmail.com>
2021-12-09 00:39:03 +0100

    js version 1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,34 @@
 const profile = document.querySelector(".profile");
-const popupprofile = document.querySelector(".popupprofile");
-const profilenametext = profile.querySelector(".profile__name-text"); // Элемент h1 с именем профиля
-const profileprofessiontext = profile.querySelector(
-  ".profile__profession-text"
-); // Элемент страницы с названием профессии
-const buttonprofileopen = profile.querySelector(".profile__edit-button"); // Кнопка отрытия popup профиля
-const buttonprofileclose = popupprofile.querySelector(".popup__close"); // Кнопка закрытия popup профиля
-const buttonprofilesave = popupprofile.querySelector(".popup__button-save"); // Кнопка сохранения popup профиля
-const formeditprofile = popupprofile.querySelector(".edit-profile"); // Форма редактирования popup профиля
-const inputprofilename = popupprofile.querySelector(".inputprofilename"); // Текстовое поле для ввода имени профиля
-const inputprofileprofession = popupprofile.querySelector(".inputprofileprofession"); // Текстовое поле для ввода профессии
-
-let profilename = profilenametext.textContent; // Имя профиля со страницы из тега h1
-let profileprofession = profileprofessiontext.textContent; // Профессия со страницы
+const popupProfile = document.querySelector(".popupprofile");
+const profileNameText = profile.querySelector(".profile__name-text"); // Элемент h1 с именем профиля
+const profileProfessionText = profile.querySelector(".profile__profession-text"); // Элемент страницы с названием профессии
+const buttonProfileOpen = profile.querySelector(".profile__edit-button"); // Кнопка отрытия popup профиля
+const buttonProfileClose = popupProfile.querySelector(".popup__close"); // Кнопка закрытия popup профиля
+const buttonProfileSave = popupProfile.querySelector(".popup__button-save"); // Кнопка сохранения popup профиля
+const formEditProfile = popupProfile.querySelector(".edit-profile"); // Форма редактирования popup профиля
+const inputProfileName = popupProfile.querySelector(".inputprofilename"); // Текстовое поле для ввода имени профиля
+const inputProfileProfession = popupProfile.querySelector(".inputprofileprofession"); // Текстовое поле для ввода профессии
 
 // Открываем popup окно редактирования профиля
 // Подставляем значения в поля input со страницы сайта
-buttonprofileopen.addEventListener("click", function () {
-  popupprofile.classList.add("popup_opened");
-  inputprofilename.value = profilename;
-  inputprofileprofession.value = profileprofession;
+buttonProfileOpen.addEventListener("click", function () {
+  popupProfile.classList.add("popup_opened");
+  inputProfileName.value = profileNameText.textContent;;
+  inputProfileProfession.value = profileProfessionText.textContent;;
 });
 
-// Закрываем popup окно редактирования профиля
-buttonprofileclose.addEventListener("click", function () {
-  popupprofile.classList.remove("popup_opened");
+// Закрываем popup окно редактирования профиля без редактирования, крестиком
+buttonProfileClose.addEventListener("click", function () {
+  popupProfile.classList.remove("popup_opened");
 });
 
-// Функция закрытия окна редактирования профиля
-// Получаем и Передаём значения полей input в html
-// Закрываем окно
-function formeditprofession (evt) {
+// Функция закрытия окна редактирования с сохранением изменений
+function editFormProfession (evt) {
 evt.preventDefault();
-profilename = inputprofilename.value;
-profileprofession = inputprofileprofession.value;
-profilenametext.textContent = profilename;
-profileprofessiontext.textContent = profileprofession;
-popupprofile.classList.remove("popup_opened");
+profileNameText.textContent = inputProfileName.value;
+profileProfessionText.textContent = inputProfileProfession.value;
+popupProfile.classList.remove("popup_opened");
 }
 
 // Отслеживаем нажатие кнопки "Сохранить" в форме редактирования профиля
-// Запускаем закрытие с сохранением данных
-buttonprofilesave.addEventListener('click', formeditprofession);
+formEditProfile.addEventListener('submit', editFormProfession);
